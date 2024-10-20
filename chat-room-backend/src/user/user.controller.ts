c55f6612dd7aa81426f91fedebde5880561f2352
yunumu <1416900346@qq.com>
2024-10-20 20:27:41 +0800

    feat: 初始化 Nest.js 项目、prisma、注册

---
+++ chat-room-backend/src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  async register(@Body() registerUser: RegisterUserDto) {
+    delete registerUser.captcha;
+
+    return await this.userService.create(registerUser);
+  }
+}
---
+++ chat-room-backend/src/user/user.service.ts
@@ -0,0 +1,18 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class UserService {
+  @Inject(PrismaService)
+  private prisma: PrismaService;
+
+  async create(data: Prisma.UserCreateInput) {
+    return await this.prisma.user.create({
+      data,
+      select: {
+        id: true,
+      },
+    });
+  }
+}
